4e184f603777c4c22942086e7ed16b0587adc9bb
metanmai <103983719+metanmai@users.noreply.github.com>
2024-03-22 19:46:49 +0530

    Initialized project

---
+++ src/test/java/com/tanmai/kiranaregister/KiranaRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmai.kiranaregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiranaRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
